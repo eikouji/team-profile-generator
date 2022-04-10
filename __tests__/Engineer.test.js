f65ea906dc9f4ba3034a3a7daae5f12c3f9a0ce4
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-10 14:56:52 -0700

    adding engineer test, engineer functions

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,20 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
 test("puts Github account as a constructor", () => {
-    const GitHubUser = "GithubUser";
-    const employee = new Engineer("")
+    const gitHubUser = "GithubUser";
+    const employee = new Engineer("angela", 1, "email@test.com", gitHubUser);
     expect(employee.github).toBe(testValue);
 
-}
-
-test("getRole() should return ${"Engineer"}, () => {
-    const testValue = "Engineer";
-    const employee = new Engineer("John", 1, "email@test.com", "GithubUser");
-    expect(employee.getRole()).to.equal(testValue);
 });
 
-test("Can get GitHub username from gitHub()", () => {
-    const testValue = "GitHubUser";
-    const employee = new Engineer("John", 1, "email@test.com", testValue)
-  expect(employee.getGithub()).toBe(testValue);
+test("can get GitHub username from getGithub()", () => {
+    const username = "GitHubUser";
+    const employee = new Engineer("angela", 1, "email@test.com", username);
+
+    expect.(employee.getGithub()).toBe(username);
+})l;
+
+test("getRole() should return \"Engineer\"", () => {
+    const engineer = "Engineer",
+    const employee = new Engineer("angela", 1, "email@test.com", "GitHubUser");
+    expect.(employee.getRole()).toBe(engineer);
 });
