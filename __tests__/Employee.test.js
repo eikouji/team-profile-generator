012f003ef6d2b22f428dcef17039627dd5fc8ee3
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-10 14:10:38 -0700

    initial files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+// Employee parent class //
+
+test('')
+
+// Clicking on Employee will prompt from team manager, engineer, or intern //
+
+// each employee will have name, id, email, Github username, and taken back to menu after //
+
+// when done building team, HTML is generated //
