3d15bce8cdaa353e2fa2c26b0ee74df712089085
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-10 15:36:02 -0700

    added Manager.js, manager.test.js

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/Manager");
+
+test("can set office number by constructor argument", () => {
+    const managerOfficeNumber = 100;
+    const employee = new Manager("Tristan", 1, "test@email.com", managerOfficeNumber);
+    expect(employee.OfficeNumber).toBe(managerOfficeNumber);
+});
+
+test("can get office number with getOfficeNumber()", () => {
+    const officeNumber = 100;
+    const employee = newManager("Tristan", 1, "test@email.com", officeNumber);
+
+  expect(employee.getOfficeNumber()).toBe(officeNumber);
+});
+
+test("getRole() should return \"Manager\"", () => {
+    const teamManager = "Manager";
+    const employee = new Manager("Tristan", 1, "test@email.com", 100);
+    expect(employee.getRole()).toBe(teamManager);
+});
